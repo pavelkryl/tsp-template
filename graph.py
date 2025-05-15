27bac819328bb9db141899482ac50e5df2c65dad
Pavel Kryl <pavel@kryl.eu>
2025-05-15 07:31:12 +0200

    Delete graph.py

--- graph.py
+++
@@ -1,7 +0,0 @@
-class Graph:
-    
-    def new_edge(self, from_city: str, to_city: str, distance: int) -> None:
-        ...
-
-    def find_shortest_path(self, from_city: str, to_city: str) -> int:
-        ...
